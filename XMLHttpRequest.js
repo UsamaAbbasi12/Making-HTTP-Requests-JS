46b50cacf158bfbb546259815123c70dbc058d24
UsamaAbbasi12 <usamaabbasi199@gmail.com>
2022-01-14 10:29:44 +0500

    third

--- XMLHttpRequest.js
+++ XMLHttpRequest.js
@@ -3,10 +3,21 @@ const myReq = new XMLHttpRequest();
 myReq.addEventListener("load", function () {
   const data = JSON.parse(this.responseText);
   const allData = data.results;
-  allData.map((dat) => {
-    console.log(dat);
+  const filmUrl = data.results[0].films[0];
+  console.log("This is Data from first Request!");
+  console.log(allData);
+
+  const secondReq = new XMLHttpRequest();
+  secondReq.addEventListener("load", function () {
+    console.log("This is Data from second Request!");
+    const filmData = JSON.parse(this.responseText);
+    console.log(filmData.title);
   });
-  //   console.log("it worked!", data.results);
+  secondReq.addEventListener("error", function (e) {
+    console.log("Error: " + e);
+  });
+  secondReq.open("GET", filmUrl);
+  secondReq.send();
 });
 
 myReq.addEventListener("error", () => {
@@ -14,5 +25,6 @@ myReq.addEventListener("error", () => {
 });
 //Method to get data from an API
 myReq.open("GET", "https://swapi.dev/api/planets/");
+console.log("request sent");
 myReq.send();
 console.log(myReq.responseText);
